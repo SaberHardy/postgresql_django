7ac3ea8b03cc4475d6150a1110211d682374006c
SaberHardy <samirsamado5@gmail.com>
2022-02-12 18:12:45 +0300

    create models and home page

--- cars/models.py
+++ cars/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Driver(models.Model):
+    name = models.CharField(max_length=30)
+    license = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.DateField()
+    vin = models.CharField(max_length=100)
+    owner = models.ForeignKey(Driver, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.make
+
+
--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+from cars.models import Driver, Car
+
+
+def car_detail(request, pk):
+    driver = Driver.objects.get(pk=pk)
+    cars = Car.objects.filter(owner_id=driver.id)
+
+    context = {
+        'cars': cars,
+        'driver': driver,
+    }
+
+    return render(request, 'home.html', context)
